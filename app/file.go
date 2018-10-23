4ba00061b393baac45744406f858b5ba917f1b27
Namiko Toriyama <>
2018-10-23 20:05:42 +0900

    add scanner

--- app/file.go
+++ app/file.go
@@ -1,12 +1,25 @@
 package app
 
 import (
+	"bufio"
 	"context"
+	"fmt"
 	"io"
+	"strconv"
+	"strings"
 
 	"cloud.google.com/go/storage"
 	"google.golang.org/appengine/log"
 )
+
+// MoneyItem is the items of csv.
+type MoneyItem struct {
+	numberID int
+	product  string
+	company  string
+	value    int
+	stock    int
+}
 
 // GetFile fetches GCS upload file.
 func GetFile(ctx context.Context, filename string, bucketID string) error {
@@ -35,16 +48,59 @@ func GetFile(ctx context.Context, filename string, bucketID string) error {
 		}
 	}
 	defer closer(reader)
-	parseLog(reader)
 
-	// info, err := ninjalog.Parse(filename, reader)
-	// if err != nil {
-	// 	log.Errorf(ctx, "failed to parse ninjalog: %v", err)
-	// 	return nil, err
-	// }
+	info, err := parseLog(reader, ctx)
+	if err != nil {
+		log.Errorf(ctx, "failed to parse ninjalog: %v", err)
+		return err
+	}
+	log.Debugf(ctx, "info: %v", info)
 	return nil
 }
 
-func parseLog(info io.Reader) {
+func parseLog(info io.Reader, ctx context.Context) (*[]MoneyItem, error) {
+	scanner := bufio.NewScanner(info)
+	items := []MoneyItem{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "number") {
+			continue
+		} else {
+			log.Debugf(ctx, "line: %v", line)
+			item, err := logItems(line)
+			if err != nil {
+				return nil, err
+			}
+			items = append(items, *item)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return &items, nil
+}
 
+func logItems(line string) (*MoneyItem, error) {
+
+	lineitems := strings.Split(line, ",")
+	numberIDItem, err := strconv.Atoi(lineitems[0])
+	if err != nil {
+		return nil, fmt.Errorf("failed to prefix number to int: %v", err)
+	}
+	valueItem, err := strconv.Atoi(lineitems[3])
+	if err != nil {
+		return nil, fmt.Errorf("failed to prefix value to int: %v", err)
+	}
+	stockItem, err := strconv.Atoi(lineitems[4])
+	if err != nil {
+		return nil, fmt.Errorf("failed to prefix stock to int: %v", err)
+	}
+	items := &MoneyItem{
+		numberID: numberIDItem,
+		product:  lineitems[1],
+		company:  lineitems[2],
+		value:    valueItem,
+		stock:    stockItem,
+	}
+	return items, nil
 }
--- app/file_test.go
+++ app/file_test.go
@@ -1,14 +1,26 @@
 package app
 
 import (
-	"strings"
+	"bytes"
 	"testing"
 )
 
 func TestParseLog(t *testing.T) {
 	testdata := "number,product,company,value,stock\n1,game,DeNA,1000,8000000\n2,chromebook,google,40000,200000\n3,book,amazon,3000,50000000"
+	r := bytes.NewReader([]byte(testdata))
+	itemdatas, err := parseLog(r)
+	if err != nil {
+		t.Errorf("parseLog(%v)=%v, got=%v", testdata, err, itemdatas)
+	}
+	t.Logf("itemdatas=%v", itemdatas)
 
-	r := strings.NewReader(testdata)
-	parseLog(r)
+}
+func TestLogItems(t *testing.T) {
+	testdata := "1,game,DeNA,1000,8000000"
+	itemdata, err := logItems(testdata)
+	if err != nil {
+		t.Errorf("logItems(%v)=%v: got=%v", testdata, err, itemdata)
+	}
+	t.Logf("itemdata=%v", itemdata)
 
 }
